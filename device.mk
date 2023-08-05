612e96ae23111741e36572cce0d975940ac92670
MrFluffyOven <137286588+MrFluffyOven@users.noreply.github.com>
2023-08-05 14:10:48 +0100

    Add files via upload

--- device.mk
+++ device.mk
@@ -6,3 +6,38 @@
 #
 
 LOCAL_PATH := device/samsung/gta8wifi
+
+PRODUCT_PACKAGES += \
+    android.hardware.fastboot@1.0-impl-mock \
+    fastbootd
+
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+PRODUCT_SHIPPING_API_LEVEL := 30
+
+TARGET_OTA_ASSERT_DEVICE := gta8wifi
+
+TARGET_COPY_OUT_VENDOR := vendor
+
+TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
+# TWRP specific build flags
+TW_THEME := portrait_hdpi
+RECOVERY_SDCARD_ON_DATA := true
+TW_EXCLUDE_DEFAULT_USB_INIT := true
+TW_EXTRA_LANGUAGES := true
+TW_INCLUDE_NTFS_3G := true
+TW_USE_TOOLBOX := true
+TW_INCLUDE_RESETPROP := true
+TW_INPUT_BLACKLIST := "hbtp_vm"
+TW_BRIGHTNESS_PATH := "/sys/class/backlight/panel0-backlight/brightness"
+TW_DEFAULT_BRIGHTNESS := 1200
+TARGET_USES_MKE2FS := true
+TW_NO_LEGACY_PROPS := true
+TW_USE_NEW_MINADBD := true
+TW_NO_BIND_SYSTEM := true
+TW_NO_SCREEN_BLANK := true
+TW_EXCLUDE_APEX := true
+TW_FRAMERATE := 60
+TW_DEVICE_VERSION := smiley​
+TW_HAS_DOWNLOAD_MODE := true​
+
---
+++ twrp_gta8wifi.mk
@@ -0,0 +1,29 @@
+#
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
+
+# Inherit from gta8wifi device
+$(call inherit-product, device/samsung/gta8wifi/device.mk)
+
+PRODUCT_DEVICE := gta8wifi
+PRODUCT_NAME := twrp_gta8wifi
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := SM-X200
+PRODUCT_MANUFACTURER := samsung
+
+PRODUCT_GMS_CLIENTID_BASE := android-samsung-ss
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="gta8wifixx-user 11 RP1A.200720.012 X200XXS2CWD2 release-keys"
+
+BUILD_FINGERPRINT := samsung/gta8wifixx/gta8wifi:11/RP1A.200720.012/X200XXS2CWD2:user/release-keys
